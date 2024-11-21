52106c7869259135bd814bf6252a6b1d0ddaa8bd
arielherself <arielherself@duck.com>
2024-11-21 12:30:11 +0800

    feat(mp4): implement edit list box and track header parser - refactor debug file walker - struct `TrackHeaderBox`, `EditListBox`

--- bin/vss.rs
+++ bin/vss.rs
@@ -1,36 +1,75 @@
 use std::fs::File;
 
-fn main() {
-    let mut file = File::open("ref/test.mp4").unwrap();
-    while let Ok(bx) = mp4::read_box(&mut file) {
-        eprintln!("Read box: size = {}, name = {}", bx.size, bx.name);
+struct MpegFileWalker {
+    timescale: u32,
+}
+
+impl MpegFileWalker {
+    fn box_info_handler(&mut self, bx: mp4::MpegBox, tabs: usize) -> bool {
+        let padding = vec!["  "; tabs].concat();
         match bx.name.as_str() {
             "ftyp" => {
                 let ftyp = mp4::FileTypeBox::from(bx);
-                eprint!("  Major brand = {}\n  Minor version = {}\n  Compatible brands = {:?}\n",
+                eprint!("{padding}Major brand = {}\n{padding}Minor version = {}\n{padding}Compatible brands = {:?}\n",
                     ftyp.major_brand,
                     ftyp.minor_version,
-                    ftyp.compatible_brands
+                    ftyp.compatible_brands,
                 );
+                false
             },
-            "moov" => {
-                let mut reader = std::io::Cursor::new(bx.data);
-                while let Ok(sbx) = mp4::read_box(&mut reader) {
-                    eprintln!("  Read nested box: size = {}, name = {}", sbx.size, sbx.name);
-                    if sbx.name == "mvhd" {
-                        let mvhd = mp4::MovieHeaderBox::from(mp4::MpegFullBox::from(sbx));
-                        eprint!("    Creation time = {}\n    Modification time = {}\n    Timescale = {}\n    Duration = {:?}\n    Rate = {}\n    Volume = {}\n",
-                            mvhd.creation_time,
-                            mvhd.modification_time,
-                            mvhd.timescale,
-                            mp4::misc::parse_duration(mvhd.duration, mvhd.timescale),
-                            mvhd.rate as f32 / 65536.0,
-                            mvhd.volume as f32 / 256.0,
-                        );
-                    }
-                }
+            "moov" => true,
+            "mvhd" => {
+                let mvhd = mp4::MovieHeaderBox::from(mp4::MpegFullBox::from(bx));
+                self.timescale = mvhd.timescale;
+                eprint!("{padding}Creation time = {}\n{padding}Modification time = {}\n{padding}Timescale = {}\n{padding}Duration = {:?}\n{padding}Rate = {}\n{padding}Volume = {}\n",
+                    mvhd.creation_time,
+                    mvhd.modification_time,
+                    mvhd.timescale,
+                    mp4::misc::parse_duration(mvhd.duration, mvhd.timescale),
+                    mvhd.rate as f32 / 65536.0,
+                    mvhd.volume as f32 / 256.0,
+                );
+                false
             },
-            _ => (),
+            "trak" => true,
+            "tkhd" => {
+                let tkhd = mp4::TrackHeaderBox::from(mp4::MpegFullBox::from(bx));
+                eprint!("{padding}Creation time = {}\n{padding}Modification time = {}\n{padding}Track ID = {}\n{padding}Duration = {:?}\n{padding}Volume = {}\n{padding}Width = {}\n{padding}Height = {}\n",
+                    tkhd.creation_time,
+                    tkhd.modification_time,
+                    tkhd.track_id,
+                    mp4::misc::parse_duration(tkhd.duration, self.timescale),
+                    tkhd.volume as f32 / 256.0,
+                    tkhd.width as f32 / 65536.0,
+                    tkhd.height as f32 / 65536.0,
+                );
+                false
+            }
+            _ => false,
         }
     }
+
+    fn print_box_info(&mut self, bx: mp4::MpegBox, tabs: usize) {
+        let padding = vec!["  "; tabs].concat();
+        eprintln!("{padding}Read box: size = {}, name = {}", bx.size, bx.name);
+        if self.box_info_handler(bx.to_owned(), tabs + 1) {
+            let mut reader = std::io::Cursor::new(bx.data);
+            while let Ok(sbx) = mp4::read_box(&mut reader) {
+                self.print_box_info(sbx, tabs + 1);
+            }
+        }
+    }
+
+    fn print_file_info(&mut self, reader: &mut dyn std::io::Read) {
+        while let Ok(bx) = mp4::read_box(reader) {
+            self.print_box_info(bx, 0);
+        }
+    }
+}
+
+
+fn main() {
+    let mut file = File::open("ref/test.mp4").unwrap();
+    let mut walker = MpegFileWalker { timescale: 1 };
+    walker.print_file_info(&mut file);
 }
--- crates/mp4/src/lib.rs
+++ crates/mp4/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod misc;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct MpegBox {
     pub size: u64,
     pub name: String,
@@ -8,10 +8,11 @@ pub struct MpegBox {
     pub data: Vec<u8>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct MpegFullBox {
     pub size: u64,
     pub name: String,
+    /// only usertype box has this field
     pub usertype: Option<[u8; 16]>,
     pub version: u8,
     pub flags: [u8; 3],
@@ -66,9 +67,8 @@ pub fn read_box(input: &mut dyn std::io::Read) -> Result<MpegBox, Box<dyn std::e
     Ok( MpegBox { size, name, usertype, data } )
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FileTypeBox {
-    pub usertype: Option<[u8; 16]>,
     pub major_brand: String,
     pub minor_version: u32,
     pub compatible_brands: Vec<String>,
@@ -77,7 +77,6 @@ pub struct FileTypeBox {
 impl From<MpegBox> for FileTypeBox {
     fn from(value: MpegBox) -> Self {
         Self {
-            usertype: value.usertype,
             major_brand: misc::parse_utf8(&value.data[0..4]),
             minor_version: misc::parse_u32(&value.data[4..8]),
             compatible_brands: value.data[8..].chunks(4).map(misc::parse_utf8).collect(),
@@ -85,9 +84,8 @@ impl From<MpegBox> for FileTypeBox {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct MovieHeaderBox {
-    pub usertype: Option<[u8; 16]>,
     pub flags: [u8; 3],
     pub version: u8,
     pub creation_time: u64,
@@ -133,7 +131,6 @@ impl From<MpegFullBox> for MovieHeaderBox {
         let pre_defined: [u32; 6] = value.data[base + 52..base + 52 + 6 * 4].chunks(4).map(misc::parse_u32).collect::<Vec<u32>>().try_into().unwrap();
         let next_track_id = misc::parse_u32(&value.data[76..80]);
         Self {
-            usertype: value.usertype,
             flags: value.flags,
             version: value.version,
             creation_time,
@@ -150,3 +147,138 @@ impl From<MpegFullBox> for MovieHeaderBox {
         }
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct TrackHeaderBox {
+    pub flags: [u8; 3],
+    pub version: u8,
+    pub creation_time: u64,
+    pub modification_time: u64,
+    pub track_id: u32,
+    pub reserved_a: u32,
+    pub duration: u64,
+    pub reserved_b: [u32; 2],
+    pub layer: i16,
+    pub alternate_group: i16,
+    /// 8.8 float
+    pub volume: i16,
+    pub reserved_c: u16,
+    pub matrix: [i32; 9],
+    // 16.16 float
+    pub width: i32,
+    // 16.16 float
+    pub height: i32,
+}
+
+impl From<MpegFullBox> for TrackHeaderBox {
+    fn from(value: MpegFullBox) -> Self {
+        let creation_time;
+        let modification_time;
+        let track_id;
+        let reserved_a;
+        let duration;
+        let base;
+        if value.version == 1 {
+            creation_time = misc::parse_u64(&value.data[0..8]);
+            modification_time = misc::parse_u64(&value.data[8..16]);
+            track_id = misc::parse_u32(&value.data[16..20]);
+            reserved_a = misc::parse_u32(&value.data[20..24]);
+            duration = misc::parse_u64(&value.data[24..32]);
+            base = 32;
+        } else {
+            creation_time = misc::parse_u32(&value.data[0..4]) as u64;
+            modification_time = misc::parse_u32(&value.data[4..8]) as u64;
+            track_id = misc::parse_u32(&value.data[8..12]);
+            reserved_a = misc::parse_u32(&value.data[12..16]);
+            duration = misc::parse_u32(&value.data[16..20]) as u64;
+            base = 20;
+        }
+        let reserved_b: [u32; 2] = value.data[base..base + 8].chunks(4).map(misc::parse_u32).collect::<Vec<u32>>().try_into().unwrap();
+        let layer = misc::parse_i16(&value.data[base + 8..base + 10]);
+        let alternate_group = misc::parse_i16(&value.data[base + 10..base + 12]);
+        let volume = misc::parse_i16(&value.data[base + 12..base + 14]);
+        let reserved_c = misc::parse_u16(&value.data[base + 14..base + 16]);
+        let matrix: [i32; 9] = value.data[base + 16..base + 16 + 9 * 4].chunks(4).map(misc::parse_i32).collect::<Vec<i32>>().try_into().unwrap();
+        let width = misc::parse_i32(&value.data[base + 52..base + 56]);
+        let height = misc::parse_i32(&value.data[base + 56..base + 60]);
+        Self {
+            flags: value.flags,
+            version: value.version,
+            creation_time,
+            modification_time,
+            track_id,
+            reserved_a,
+            duration,
+            reserved_b,
+            layer,
+            alternate_group,
+            volume,
+            reserved_c,
+            matrix,
+            width,
+            height,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct EditListEntry {
+    pub segment_duration: u64,
+    pub media_time: i64,
+    pub media_rate_integer: i16,
+    pub media_rate_fraction: i16,
+}
+
+#[derive(Debug, Clone)]
+pub struct EditListBox {
+    pub version: u8,
+    pub entry_count: u32,
+    pub entries: Vec<EditListEntry>,
+}
+
+impl From<MpegFullBox> for EditListBox {
+    fn from(value: MpegFullBox) -> Self {
+        let entry_count = misc::parse_u32(&value.data[0..4]);
+        let mut entries = vec![];
+        let mut base = 4;
+        for _ in 0..entry_count {
+            let segment_duration;
+            let media_time;
+            if value.version == 1 {
+                segment_duration = misc::parse_u64(&value.data[base..base + 8]);
+                media_time = misc::parse_i64(&value.data[base + 8..base + 16]);
+                base += 16;
+            } else {
+                segment_duration = misc::parse_u32(&value.data[base..base + 4]) as u64;
+                media_time = misc::parse_i32(&value.data[base + 4..base + 8]) as i64;
+                base += 8;
+            }
+            let media_rate_integer = misc::parse_i16(&value.data[base..base + 2]);
+            let media_rate_fraction = misc::parse_i16(&value.data[base + 2..base + 4]);
+            entries.push(EditListEntry {
+                media_time,
+                segment_duration,
+                media_rate_integer,
+                media_rate_fraction,
+            })
+        }
+        Self {
+            version: value.version,
+            entry_count,
+            entries,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct EditBox {
+    pub edit_list: Option<EditListBox>,
+}
+
+#[derive(Debug, Clone)]
+pub struct TrackBox {
+    pub track_header: TrackHeaderBox,
+    pub edit_lists: Option<EditBox>,
+}
+
+// TODO: parse TrackBox
